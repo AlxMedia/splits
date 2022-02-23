450ecf3592b8b1a0adc22b4655de0fb7612d0b57
AlxMedia <contact@alx.media>
2022-02-23 12:10:39 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -561,7 +561,7 @@ Kirki::add_field( 'split_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'split' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'split' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'split' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'split' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'split' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'split' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'split' ),
@@ -578,7 +578,7 @@ Kirki::add_field( 'split_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'split' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'split' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'split' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'split' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'split' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
